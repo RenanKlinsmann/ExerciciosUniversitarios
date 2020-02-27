cd104e99c21eb6a49ab94e4604b6ee8f945351e4
RenanKlinsmann <57994919+RenanKlinsmann@users.noreply.github.com>
2020-02-26 23:40:48 -0300

    Update TedUmAlana.java

--- TedUmAlana.java
+++ TedUmAlana.java
@@ -7,46 +7,46 @@
 ° salário líquido.
 ° o valor descontado.*/
 
+package tedSalarioDescontos;
+
 import java.util.Scanner;
 
-public class Main
-{
+class SalarioLiquido {
+
 	public static void main(String[] args) {
-	    
-	        Scanner sc = new Scanner(System.in);
-	        System.out.println("Digite as horas trabalhadas:");
-	        double horas_trab = sc.nextDouble();
-	        System.out.println("Digite o valor da hora trabalhada:");
-	        double valor_hora = sc.nextDouble();
-	    
-	    
-	        double salario_bruto = horas_trab * valor_hora;
-	        double descontoIprf = (salario_bruto * 11)/100;
-		double descontoInss = (salario_bruto * 8)/100;
-		double descontoSind = (salario_bruto * 5)/100;
-		
-		System.out.println("Salario Bruto = R$"+ salario_bruto);
-		System.out.println("IPRF: 11% = R$"+ descontoIprf);
-		System.out.println("INSS: 8% = R$"+ descontoInss);
-		System.out.println("SINDICATO: 5% = R$"+ descontoSind);
-		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o mês e ano referente:");
+		String mes_referente = sc.nextLine();
+		System.out.println("Digite o valor da hora trabalhada R$:");
+		double valor_hora = sc.nextDouble();
+		System.out.println("Digite a quantidade de horas trabalhadas:");
+		double horas_trab = sc.nextDouble();
+
+		double salario_bruto = horas_trab * valor_hora;
+		double descontoIprf = (salario_bruto * 11) / 100;
+		double descontoInss = (salario_bruto * 8) / 100;
+		double descontoSind = (salario_bruto * 5) / 100;
+
+		System.out.println("Salario referente a: " + mes_referente);
+		System.out.println("Salario Bruto = R$" + salario_bruto);
+		System.out.println("IPRF: 11% = R$" + descontoIprf);
+		System.out.println("INSS: 8% = R$" + descontoInss);
+		System.out.println("SINDICATO: 5% = R$" + descontoSind);
+
 		double total_impostos = descontoIprf + descontoInss + descontoSind;
-		
+
 		System.out.printf("Total de impostos = R$ %.2f", total_impostos);
-		
+
 		double salario_liquido = salario_bruto;
-		if(salario_bruto > 0){
-		  salario_liquido =  salario_bruto - descontoIprf - descontoInss - descontoSind;
-		  System.out.printf("\nSalario liquido = R$ %.2f", salario_liquido);
-		 
-		}else if(salario_bruto <= 0){
-		    System.out.println("ISENTO");
+		if (salario_bruto > 0) {
+			salario_liquido = salario_bruto - descontoIprf - descontoInss - descontoSind;
+			System.out.printf("\nSalario liquido = R$ %.2f", salario_liquido);
+
+		} else if (salario_bruto <= 0) {
+			System.out.println("ISENTO");
 		}
-		
-		
-		
-		
-		
+
 		sc.close();
 	}
 }
