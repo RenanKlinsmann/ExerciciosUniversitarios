76d02912d2c813e860a7dab9c40967c263ed96b7
RenanKlinsmann <57994919+RenanKlinsmann@users.noreply.github.com>
2020-03-06 15:43:16 -0300

    Conversor_De_Moedas
    Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe CurrencyConverter para ser responsável pelos cálculos.

--- Programa.java
+++ Programa.java
@@ -3,26 +3,29 @@ package aplication;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entidades.Triangulo;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+		Triangulo x, y;//declarando a variavel pela classe do pacote entidade
+		x = new Triangulo();// instanciado as variaveis da classe triangulo
+		y = new Triangulo();
+		
 		System.out.println("Digite as medidas do triangulo X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();//dessa forma acesso os atributos da classe triangulo usando a variavel do meu programa principal.variavel declarada na classe triangulo
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Digite as medidas do triangulo Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
 		
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		double areaX = x.area();//chamando a funcao area criada na classe triangulo
+		double areaY = y.area();
 		
 		System.out.printf("Area do triangulo X: %.4f%n", areaX);
 		System.out.printf("Area do triangulo Y: %.4f%n", areaY);
