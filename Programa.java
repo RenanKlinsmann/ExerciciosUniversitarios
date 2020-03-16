0ea9beb4231e664a058c47f1f07bf9079113506c
RenanKlinsmann <57994919+RenanKlinsmann@users.noreply.github.com>
2020-03-15 23:23:47 -0300

    UdemyExercicioDeFixacaoMatrizes
    Fazer um programa para ler dois números inteiros M e N, e depois ler uma matriz de M linhas por N colunas contendo números inteiros, podendo haver repetições. Em seguida, ler um número inteiro X que pertence à matriz. Para cada ocorrência de X, mostrar os valores à esquerda, acima, à direita e abaixo de X, quando houver, conforme exemplo.
    Example
    3 4 
    10 8 15 12 
    21 11 23 8 
    14 5 13 19 
    8 
    Position 0,1: 
    Left: 10 
    Right: 15 
    Down: 11 
    Position 1,3: 
    Left: 23 
    Up: 12 
    Down: 19

--- Programa.java
+++ Programa.java
@@ -1,56 +1,80 @@
 package aplicacao;
 
-import java.util.Locale;
 import java.util.Scanner;
-
-import entidades.Conta;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		Conta conta;
-		
-		System.out.print("Insira o numero da conta:");
-		int numero = sc.nextInt();
-		System.out.print("Insira o nome do titular da conta:");
-		sc.nextLine();
-		String nome = sc.nextLine();
-		System.out.print("Deseja fazer um deposito inicial (S/N)? ");
-		char resposta = sc.next().charAt(0);
-		if (resposta == 's') {
-			System.out.print("Insira o valor de deposito inicial: ");
-			double depositoInicial = sc.nextDouble();
-			conta = new Conta(numero, nome, depositoInicial);
-		}
-		else {
-			conta = new Conta(numero, nome);
-		}
-		
-	    System.out.println();
-	    System.out.println("Dados do cliente:");
-	    System.out.println(conta);
-	    
-	    System.out.println();
-        System.out.print("Insira o valor a ser depositado $: ");
-        double valorDepositado = sc.nextDouble();
-        conta.deposito(valorDepositado);
-        System.out.println("Saldo atualizado:");
-        System.out.println(conta);
-        
-        System.out.println();
-        System.out.print("Insira o valor de Saque: $ ");
-        double valorSaque = sc.nextDouble();
-        conta.saque(valorSaque);
-        System.out.println("Saldo atualizado: $ ");
-        System.out.println(conta);
-		
+    Scanner sc = new Scanner(System.in);
 
 		
+
+		int m = sc.nextInt();
+
+		int n = sc.nextInt();
+
+		int[][] mat = new int[m][n];
+
 		
+
+		for (int i=0; i<mat.length; i++) {
+
+			for (int j=0; j<mat[i].length; j++) {
+
+				mat[i][j] = sc.nextInt();
+
+			}
+
+		}
+
 		
+
+		int x = sc.nextInt();
+
+		
+
+		for (int i=0; i<mat.length; i++) {
+
+			for (int j=0; j<mat[i].length; j++) {
+
+				if (mat[i][j] == x) {
+
+					System.out.println("Posição: " + i + "," + j);
+
+					if (j > 0) {
+
+						System.out.println("Esquerda: " + mat[i][j-1]);
+
+					}
+
+					if (i > 0) {
+
+						System.out.println("Acima: " + mat[i-1][j]);
+
+					}
+
+					if (j < mat[i].length-1) {
+
+						System.out.println("Direita: " + mat[i][j+1]);
+
+					}
+
+					if (i < mat.length-1) {
+
+						System.out.println("Abaixo: " + mat[i+1][j]);
+
+					}
+
+				}
+
+			}
+
+		}
+
+		
+
 		sc.close();
+
 	}
 
 }
